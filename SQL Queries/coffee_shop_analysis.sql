b6ceddb8d77a7d6a97809e487a556d7cedf29673
ziyaad-ali <mziyaad2025@gmail.com>
2025-12-29 00:10:13 +0530

    Update coffee_shop_analysis.sql

--- SQL Queries/coffee_shop_analysis.sql
+++ SQL Queries/coffee_shop_analysis.sql
@@ -1,1 +1,38 @@
+-----------------------------------------
+-- Coffee Shop Retail Sales Analysis - MS SQL Server
+-----------------------------------------
+
+-- Create Database
+CREATE DATABASE CoffeeSales;
+GO
+
+-- Use CoffeeSales
+USE CoffeeSales;
+GO
+
+-- Create table 
+CREATE TABLE SalesData (
+    transaction_id INT NOT NULL PRIMARY KEY,
+    transaction_date DATE,
+    transaction_time TIME,
+    transaction_qty INT,
+    store_id INT,
+    store_location VARCHAR(30),
+    product_id INT,
+    unit_price FLOAT,
+    product_type VARCHAR(30),
+    product_detail VARCHAR(50),
+    product_category VARCHAR(30)
+);
+GO
+-- Import csv file
+BULK INSERT SalesData
+FROM 'C:\data\Coffee Shop Sales(Transactions).csv'
+WITH (
+    FIRSTROW = 2,              -- header skip
+    FIELDTERMINATOR = ',',     -- CSV separator
+    ROWTERMINATOR = '\n',
+    TABLOCK
+);
+GO
 
